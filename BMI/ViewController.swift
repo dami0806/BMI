bd51509b6ec3257d583616c466e1afc0a0ccddf9
dami0806 <dami0804@naver.com>
2022-11-06 12:06:39 +0900

    커밋

--- BMI/ViewController.swift
+++ BMI/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet weak var weightTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        heightTextField.delegate = self
+        weightTextField.delegate = self
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func calcuBtnTapped(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+}
+extension ViewController: UITextFieldDelegate{
+    
 }
 
